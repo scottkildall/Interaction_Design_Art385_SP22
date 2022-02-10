f72280fe79ce9b09f5eb23254466ece01ff6782b
Scott Kildall <4422698+scottkildall@users.noreply.github.com>
2022-02-09 18:07:06 -0800

    working SimpleStates

--- MoodStates/MoodsExample/README.md
+++ MoodStates/MoodsExample/README.md
@@ -1,11 +1,24 @@
-## Empty Example for P5.js
+## MoodsExample.js
 #### by Scott Kildall
 #### Last updated Jan 24 2022
 
 
-
 ### Overview
-This is a modified version of the p5.js Empty Example that simplifies the directory structure
+This is a simple state machine that displays an image and a series of transitions that can lead to new states. It uses the p5.simpleStateManager.js library file, which does the heavy lifting: loading a CSV file with the various states and transitions in it.
+
+### Add to your index.html:
+
+ <script src="p5.simpleStateManager.js"></script>
+
+ This lets you use the code in the p5.simpleStateManager.js library
+
+### p5.simpleStateManager.js
+
+ This is a library that loads a csv file for images + texts + states. The implementation destails are in the source code file and here is what we need to know.
+
+### CSV file format 
+
+### library functions
 
 
 ### Technical Details
--- MoodStates/MoodsExample/p5.simpleStateManager.js
+++ MoodStates/MoodsExample/p5.simpleStateManager.js
@@ -8,9 +8,13 @@
     Loads a CSV table in the constructor
 
     SimpleStateManager variables:
-        this.statesTable        =  CSV file table, loaded in preload(), used in setup(), then not used again
-        this.currentState       =  index of the current state in the array, default to 1st state
-        this.states             =  Array of State variables
+        this.statesTable                =  CSV file table, loaded in preload(), used in setup(), then not used again
+        this.currentState               =  index of the current state in the array, default to 1st state
+        this.stateNames                 =  Array of state names
+        this.states                     =  Array of State variables, matches entries and size of state names
+        this.setImageFilenameCallback   =  Callback for image filename, when a new state is selected
+        this.setTransitionsCallback     =  Callback for image filename, when a new state is selected
+        
 
     State variables:
         this.stateName          = name of the state (for reference)
@@ -24,26 +28,79 @@ class SimpleStateManager {
     constructor(statesFilename) {
         this.statesTable = loadTable(statesFilename, 'csv', 'header');
         this.currentState = 0;   
-        this.states = [];     
-
-        console.log("start");
+        this.stateNames = [];
+        this.states = [];    
+        this.stateNames = []; 
+        this.setImageFilenameCallback = null;
+        this.setTransitionsCallback = null; 
     }
 
     // expects as .csv file with the format as outlined in the readme file
     // this will go through the states table and:
     // (1) add a new state for every unique state
     // (2) add a 
-    setup() {
+    setup(imageFilenameCallback, transitionsArrayCallback) {
         console.log(this.statesTable);
+
+        this.setImageFilenameCallback = imageFilenameCallback;
+        this.setTransitionsCallback = transitionsArrayCallback;
 
         // For each row, allocate a new state, if it is unique
         // and always add a transition
         for( let i = 0; i < this.statesTable.getRowCount(); i++ ) {
             let stateName = this.statesTable.getString(i, 'StateName');
-            console.log(stateName);
+            
+            // find the state index in the statesArray
+            let stateArrayIndex = this.stateNames.indexOf(stateName); 
+
+            console.log("index: " + i + " = " + stateName );
+            console.log("state index = " + stateArrayIndex);
+
+            // if not present, we add it
+            if( stateArrayIndex === -1 ) {
+                this.stateNames.push(stateName);
+                this.states.push(new State(stateName, this.statesTable.getString(i, 'PNGFilename')));
+                stateArrayIndex = this.states.length - 1;
+            }
+            
+            this.states[stateArrayIndex].addTransition(this.statesTable.getString(i, 'TransitionText'), this.statesTable.getString(i, 'NewState'))
+            
         }
+        
+        // All DONE
+        console.log("Setup done");
+        console.log(this.states);
+        
+        this.performCallbacks();
 
         return this.hasValidStates;
+    }
+
+    // set new state, make callbacks
+    newState(transitionsName) {
+        //console.log(transitionsName)
+        //console.log(this.states[this.currentState].transitions);
+        
+        // find the state index in the statesArray
+        let newStateIndex = this.states[this.currentState].transitions.indexOf(transitionsName); 
+        if( newStateIndex === -1 ) {
+            console.log("Error in newState(), transition not found");
+            return;
+        }
+
+        // grab the state name
+        let newStateName = this.states[this.currentState].nextStates[newStateIndex];
+        this.currentState = this.stateNames.indexOf(newStateName); 
+
+        console.log( "new state = " + newStateName);   
+
+        this.performCallbacks();
+    }
+
+    performCallbacks() {
+        // perform initial callbacks - if there is an invalid CSV, this will produce an error
+        this.setImageFilenameCallback(this.states[this.currentState].imageFilename);
+        this.setTransitionsCallback(this.states[this.currentState ].transitions);
     }
 }
 
@@ -55,7 +112,8 @@ class State {
         this.nextStates = [];
     }
 
-    addTransition() {
-
+    addTransition(transitionText,nextState) {
+        this.transitions.push(transitionText);
+        this.nextStates.push(nextState);
     }
 }
