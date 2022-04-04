9f02fb81b6eb8308ec61fe2bad50f5c717fd1a7d
Scott Kildall <4422698+scottkildall@users.noreply.github.com>
2022-04-04 14:36:00 -0700

    Basic grabbables are working

---
+++ P5Play Examples/ImprovedAvatar/sketch.js
@@ -0,0 +1,186 @@
+/***********************************************************************************
+  MultiSprite Navigation
+
+  Use of the p5.play library with a sprite class
+
+  Improvements:
+  - stop for an avatar
+  - mirror
+  - add individual speeds for different avatars
+  - set positions for each one
+  - do collision-detection
+  
+------------------------------------------------------------------------------------
+	To use:
+	Add this line to the index.html
+
+  <script src="p5.timer.js"></script>
+***********************************************************************************/
+
+// This is a speed of the 'sprite' which we can move
+var speed = 20;
+
+// an array of all the class avaatars
+var playerAvatar;
+var selectedIndex = 0;
+
+var grabbables = [];
+
+// a static sprite representing the teleporting door
+var door;
+
+// keycods for W-A-S-D
+const W_KEY = 87;
+const S_KEY = 83;
+const D_KEY = 68;
+const A_KEY = 65;
+
+// control variables for grabbables
+var overlapCount = 0;
+var preventPickup = false;        // for when you drop one
+var preventRepickup = false;      // two objects
+
+// opened 
+var opened = false;
+
+function preload() {
+ 
+  // Add new avatar animations here
+  playerAvatar = new Avatar("Player", 100, 150);
+  playerAvatar.setMaxSpeed(5);
+
+  door = new StaticSprite("Door", 900,700, 'assets/door.png');
+
+  // Add YOUR animation here
+  playerAvatar.addMovingAnimation( 'assets/run1.png', 'assets/run2.png');
+  playerAvatar.addStandingAnimation('assets/standing1.png', 'assets/standing2.png');
+
+  // Add grabbables - these appear on top of the player icon
+  grabbables.push(new StaticSprite("Star", 500,500, 'assets/fullStar.png'));
+  grabbables.push(new StaticSprite("Key", 750,200, 'assets/key.png'));
+  grabbables.push(new StaticSprite("Wheel", 400,300, 'assets/wheel.png'));
+}
+
+// Setup code goes here
+function setup() {
+  createCanvas(1000, 800);
+
+  frameRate(30);
+
+  // This will setup the animation
+  for( let i = 0; i < grabbables.length; i++ ) {
+    grabbables[i].setup();
+  }
+
+  // loads image for the door
+  door.setup();
+ }
+
+// Draw code goes here
+function draw() {
+  if( opened ) {
+    background(128,80,0);
+  }
+  else {
+    background(128);
+  }
+
+  // trap keyboard arrow keys
+  checkMovement();
+
+  // drawSprites is a function in p5.play, draws all the sprites
+  drawSprites();
+
+  // We call this to support grabbables
+  playerAvatar.update();
+
+  checkOverlaps();
+}
+
+function checkOverlaps() {
+  // We are looking for an overlap of the player avatar with the door.
+  // the 2nd argument is a local callback function, that will get called repeatedly while
+  // we may build better ov
+ playerAvatar.sprite.overlap(door.sprite, doorCollision);
+
+  // go through grabble array and set these 
+  overlapCount = 0;
+  for( let i = 0; i < grabbables.length; i++ ) {
+    playerAvatar.sprite.overlap(grabbables[i].sprite, grabbableCollision);
+  }
+
+  // we aren't overlapping, so prevent re-pickup
+  if( overlapCount === 0 ) {
+    preventPickup = false;
+  }
+  if( overlapCount === 1 ) {
+    preventRepickup = false;
+  }
+}
+
+// This will reset position
+function keyPressed() {
+  if( key === ' ') {
+    if( playerAvatar.grabbable !== undefined ) {
+      preventPickup = true;
+      playerAvatar.clearGrabbable();
+    }
+  }
+}
+
+// respond to W-A-S-D or the arrow keys
+function checkMovement() {
+  var xSpeed = 0;
+  var ySpeed = 0;
+
+  // Check x movement
+  if(keyIsDown(RIGHT_ARROW) || keyIsDown(D_KEY)) {
+    xSpeed = speed;
+  }
+  else if(keyIsDown(LEFT_ARROW) || keyIsDown(A_KEY)) {
+    xSpeed = -speed;
+  }
+  
+  // Check y movement
+  if(keyIsDown(DOWN_ARROW) || keyIsDown(S_KEY)) {
+    ySpeed = speed;
+  }
+  else if(keyIsDown(UP_ARROW) || keyIsDown(W_KEY)) {
+    ySpeed = -speed;
+  }
+
+  playerAvatar.setSpeed(xSpeed,ySpeed);
+}
+
+// When the player avatar intesects with the door, gets called repeatedly
+function doorCollision(spriteA, spriteB) {
+  if( playerAvatar.getGrabbableName() === "Key" ) {
+    opened = true;
+  }
+  else {
+    playerAvatar.setPosition(width/2, height/2);
+    opened = false;
+  }
+}
+
+// When the player avatar intesects with the door, gets called repeatedly
+// SpriteB = grabble sprite
+function grabbableCollision(spriteA, spriteB) {  
+  overlapCount++;
+
+  if( preventPickup || preventRepickup ) {
+    return;
+  }
+
+  // check for new grabble (not self)
+  if( playerAvatar.grabbable === undefined || playerAvatar.grabbable.sprite !== spriteB ) {
+    for( let i = 0; i < grabbables.length; i++ ) {
+      if( grabbables[i].sprite === spriteB ) {
+        //console.log("new set: " + i);
+        playerAvatar.setGrabbable(grabbables[i]);
+        preventRepickup = true;
+        break;
+      }
+    }    
+  }
+}
